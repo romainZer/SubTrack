5d9b0bf304c72c07eff95c8904028caae80ed63b
Romain ZERVO <romain.zervo@gmail.com>
2025-03-10 21:35:12 +0100

    Améliorations du calendrier et mise à jour des styles
    Ajout de nouveaux champs statiques pour les noms des jours de la semaine dans `CalendarComponent.xaml.cs` et ajustements des propriétés bindables. Introduction de la méthode `OnMonthChanged` pour gérer les changements de mois et création d'un pool de boutons pour réutilisation.
    
    Mise à jour des styles dans `OperationItemComponent.xaml`, augmentation de la taille des images et ajustements des étiquettes.
    
    Modification des noms des mois en minuscules dans `NumberToMonthConverter.cs` et remplacement des polices OpenSans par Montserrat dans `MauiProgram.cs` et `Styles.xaml`.
    
    Ajout d'une méthode `OnCreate` dans `MainActivity.cs` pour définir la couleur de la barre d'état sur Android.
    
    Mise à jour des couleurs dans `colors.xml` et `Colors.xaml`, avec suppression des fichiers de police OpenSans.
    
    Ajustements du BindingContext et de la structure de la page dans `CalendarPage.xaml` pour améliorer l'utilisation du contrôle `CalendarComponent`.

--- SubTrack/MauiProgram.cs
+++ SubTrack/MauiProgram.cs
@@ -11,8 +11,9 @@ namespace SubTrack
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Montserrat-Regular.ttf", "MontserratRegular");
+                    fonts.AddFont("Montserrat-SemiBold.ttf", "MontserratSemibold");
+                    fonts.AddFont("Montserrat-Bold.ttf", "MontserratBold");
                 });
 
 #if DEBUG
