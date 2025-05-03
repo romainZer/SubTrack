aba77fb9d0e171cb534dac1867368c288f2afd66
romainZer <romain.zervo@gmail.com>
2025-05-03 17:22:58 +0200

    Revert: Améliorations du calendrier et mise à jour des couleurs
    - Ajout d'un tableau statique pour les noms des jours de la semaine dans `CalendarComponent.xaml.cs`.
    - Mise à jour de la propriété `CurrentMonth` et ajout d'un pool de boutons pour les jours.
    - Introduction de la méthode `OnMonthChanged` pour gérer les changements de mois.
    - Augmentation de la taille de l'image dans `OperationItemComponent.xaml` de 30 à 35 pixels.
    - Modification des noms des mois dans `NumberToMonthConverter.cs` pour commencer par une lettre minuscule.
    - Ajout de la méthode `OnCreate` dans `MainActivity.cs` pour définir la couleur de la barre d'état sur Android.
    - Mise à jour des valeurs de couleurs dans `colors.xml` et `Colors.xaml` pour un nouveau schéma de couleurs.
    - Réorganisation de `CalendarPage.xaml` pour utiliser une grille au lieu d'un `AbsoluteLayout` et utilisation d'un `CollectionView` pour les opérations mensuelles.
    - Ajout d'un nouveau fichier image `trash_dark.png` pour le thème sombre.

--- SubTrack/Platforms/Android/MainActivity.cs
+++ SubTrack/Platforms/Android/MainActivity.cs
@@ -7,5 +7,18 @@ namespace SubTrack
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set the status bar color
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                if (Window != null)
+                {
+                    Window.SetStatusBarColor(Android.Graphics.Color.Rgb(20, 20, 20));
+                }
+            }
+        }
     }
 }
